7fe0e0b0a2c511e5f21ef3550661f7543c7ed841
adbenitez <asieldbenitez@gmail.com>
2021-09-28 23:00:31 -0400

    fix linter warnings

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     DESC = "DeltaChat/WriteFreely bridge (SimpleBot plugin)"
     KEYWORDS = "simplebot plugin deltachat writefreely bridge"
 
-    with open("README.rst") as fh:
+    with open("README.rst", encoding="utf-8") as fh:
         long_description = fh.read()
 
     setup(
@@ -61,6 +61,6 @@ if __name__ == "__main__":
             "dev": load_requirements("requirements/requirements-dev.txt"),
         },
         entry_points={
-            "simplebot.plugins": "{0} = {0}".format(MODULE_NAME),
+            "simplebot.plugins": f"{MODULE_NAME} = {MODULE_NAME}",
         },
     )
